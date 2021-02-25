40b59f662e1663e3147ba9c103bfebde023ea4c7
adrianowy <adrianoabreu.wy@gmail.com>
2021-02-24 23:51:10 -0400

    - Construção do projeto usando nextJs - criação do arquivo padrao para html, body, main - entendendo o module css do next - criação do componente profile

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/adrianowy.png" alt=""/>
+            <div>
+                <strong>Adriano de Abreu</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
